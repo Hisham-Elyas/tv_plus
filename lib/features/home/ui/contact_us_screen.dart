b399b1863c290bb0560ea2b0c2fc6d359b9a9788
HISHOôø~ <115069094+Hisham-Elyas@users.noreply.github.com>
2025-02-04 09:54:55 +0200

    Update contact_us_screen.dart

--- lib/features/home/ui/contact_us_screen.dart
+++ lib/features/home/ui/contact_us_screen.dart
@@ -1,62 +1,51 @@
 import 'package:faisal_tv/core/helpers/spacing.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_launcher_plus/flutter_launcher_plus.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../../core/localization/constants.dart';
 import '../widgets/custom_app_bar.dart';
 import '../widgets/custom_drawer_widget.dart';
 
-class ContactUsScreen extends StatefulWidget {
-  static const _channel = MethodChannel("flutter_launcher_plus");
+class ContactUsScreen extends StatelessWidget {
   const ContactUsScreen({super.key});
 
-  @override
-  State<ContactUsScreen> createState() => _ContactUsScreenState();
-}
-
-class _ContactUsScreenState extends State<ContactUsScreen> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    initPlatformState();
-  }
-
-  String _platformVersion = 'Unknown';
-
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    try {
-      platformVersion = await FlutterLauncherPlus.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
-
   Future<void> dialNumber(String number) async {
-    await ContactUsScreen._channel.invokeMethod('dialNumber', <String, String>{
-      'number': number,
-    });
+    final Uri phoneUri = Uri.parse("tel:$number");
+    if (await canLaunchUrl(phoneUri)) {
+      await launchUrl(phoneUri);
+    } else {
+      throw 'Could not dial $number';
+    }
   }
 
-  Future<void> launchUrl(String websiteUrl) async {
-    await ContactUsScreen._channel
-        .invokeMethod('launchUrl', <String, String>{'website_url': websiteUrl});
+  Future<void> sendEmail(String email) async {
+    final Uri emailUri = Uri(
+      scheme: 'mailto',
+      path: email,
+    );
+    if (await canLaunchUrl(emailUri)) {
+      await launchUrl(emailUri);
+    } else {
+      throw 'Could not send email to $email';
+    }
+  }
+
+  Future<void> openWebsite(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri, mode: LaunchMode.externalApplication);
+    } else {
+      throw 'Could not open $url';
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(title: ContactUs.tr),
-      drawer: CustomDrawerWidget(),
+      drawer: const CustomDrawerWidget(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -67,7 +56,6 @@ class _ContactUsScreenState extends State<ContactUsScreen> {
                 "0570105563",
                 style: TextStyle(
                   fontSize: 20.0.sp,
-                  height: 1.5.h,
                   fontWeight: FontWeight.bold,
                   color: Theme.of(context).colorScheme.primary,
                 ),
@@ -75,12 +63,11 @@ class _ContactUsScreenState extends State<ContactUsScreen> {
             ),
             verticalSpace(10),
             InkWell(
-              onTap: () => launchUrl("faisal2003tv@gmail.com"),
+              onTap: () => sendEmail("faisal2003tv@gmail.com"),
               child: Text(
                 "faisal2003tv@gmail.com",
                 style: TextStyle(
                   fontSize: 20.0.sp,
-                  height: 1.5.h,
                   fontWeight: FontWeight.bold,
                   color: Theme.of(context).colorScheme.primary,
                 ),
@@ -88,12 +75,11 @@ class _ContactUsScreenState extends State<ContactUsScreen> {
             ),
             verticalSpace(10),
             InkWell(
-              onTap: () => launchUrl("www.faisal-tv.com"),
+              onTap: () => openWebsite("https://www.faisal-tv.com"),
               child: Text(
                 "www.faisal-tv.com",
                 style: TextStyle(
                   fontSize: 20.0.sp,
-                  height: 1.5.h,
                   fontWeight: FontWeight.bold,
                   color: Theme.of(context).colorScheme.primary,
                 ),
